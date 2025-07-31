f464c8d06d3935d56676904e29de18c446b06a89
Subodh Rana <subodhrana390@gmail.com>
2025-07-31 12:50:42 +0545

    Database connection Fixed

--- app/db/session.py
+++ app/db/session.py
@@ -3,5 +3,8 @@ from sqlalchemy.orm import sessionmaker
 
 from app.core import DATABASE_URL
 
+if "render.com" in DATABASE_URL and "?sslmode=" not in DATABASE_URL:
+    DATABASE_URL += "?sslmode=require"
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
